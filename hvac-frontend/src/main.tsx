e704b9d6e90f674d67d9d3a11868af9344140040
Moises Sanchez <45053226+snikehero@users.noreply.github.com>
2026-02-06 21:17:37 -0600

    Theme

--- hvac-frontend/src/main.tsx
+++ hvac-frontend/src/main.tsx
@@ -1,30 +1,32 @@
-import "./index.css"
+import "./index.css";
 
-import React from "react"
-import ReactDOM from "react-dom/client"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@/components/theme-provider";
 
-import AppLayout from "@/components/layouts/AppLayout"
-import HomePage from "./pages/HomePage/Homepage"
-import { WebSocketProvider } from "./providers/WebSocketProvider"
-import AhuDetailPage from "./pages/AhuDetailPage/AhuDetailPage"
-import DashboardHVAC from "@/pages/DashboardHVAC/DashboardHVAC"
-ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-).render(
+import AppLayout from "@/components/layouts/AppLayout";
+import HomePage from "./pages/HomePage/Homepage";
+import { WebSocketProvider } from "./providers/WebSocketProvider";
+import AhuDetailPage from "./pages/AhuDetailPage/AhuDetailPage";
+import DashboardHVAC from "@/pages/DashboardHVAC/DashboardHVAC";
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-    <WebSocketProvider>
-
-      <AppLayout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/dashboardHVAC" element={<DashboardHVAC />} />
-          <Route path="/plants/:plantId/ahus/:ahuId" element={<AhuDetailPage />}
-/>
-        </Routes>
-        </AppLayout>
-        </WebSocketProvider>
+      <WebSocketProvider>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <AppLayout>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/dashboardHVAC" element={<DashboardHVAC />} />
+              <Route
+                path="/plants/:plantId/ahus/:ahuId"
+                element={<AhuDetailPage />}
+              />
+            </Routes>
+          </AppLayout>
+        </ThemeProvider>
+      </WebSocketProvider>
     </BrowserRouter>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);
